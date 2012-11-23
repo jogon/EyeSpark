ea7e260918fa17d7fcd6ea890c396527c8a2a6f0
Alex <alex.suarez100@gmail.com>
2012-11-23 16:08:29 -0400

    Stub implementation of the mappings window code

--- GazeTrackerUI/Mappings/MappingsWindow.xaml.cs
+++ GazeTrackerUI/Mappings/MappingsWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace GazeTrackerUI.Mappings
 {
@@ -18,10 +20,27 @@ namespace GazeTrackerUI.Mappings
     /// </summary>
     public partial class MappingsWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd,
+            StringBuilder buffer, int length);
+
+        private static string DefaultApplicationName = "-None Selected-";
+
         private static MappingsWindow instance;
+
+        private String selectedItem = "";
+        private Dictionary<String, String> selectedMap;
+        private Dictionary<String, String> dummyMap;
 
         private MappingsWindow()
         {
+            CreateDummyMap();
             InitializeComponent();
         }
 
@@ -43,5 +62,126 @@ namespace GazeTrackerUI.Mappings
             e.Cancel = true;
             Visibility = Visibility.Collapsed;
         }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            appComboBox.Items.Add(DefaultApplicationName);
+            appComboBox.SelectedItem = DefaultApplicationName;
+            //appComboBox.SelectedIndex = 0;
+        }
+
+        private void appComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            //string processName = (string)appComboBox.SelectedItem;
+            //if (processName == null)
+            //{
+            //    processName = dummyProfile.Id; // get the previous profile id
+            //    appComboBox.SelectedItem = processName;
+            //}
+            ////            if (!processName.Equals(DefaultApplicationName)) {
+            //AppProfile profile;
+            //if (profiles.TryGetValue(processName, out profile))
+            //{
+            //    dummyProfile = profile;
+            //}
+            //else
+            //{
+            //    dummyProfile = new AppProfile(processName);
+            //}
+            //            }
+
+            resetGestureFields();
+        }
+
+        private void resetGestureFields()
+        {
+            //upTextBox.Text = selectedMap[Gestures.Up];
+            //downTextBox.Text = selectedMap[Gestures.Down]; ;
+            //leftTextBox.Text = selectedMap[Gestures.Left]; ;
+            //rightTextBox.Text = selectedMap[Gestures.Right]; ;
+        }
+
+        private void appComboBox_DropDownOpened(object sender, EventArgs e)
+        {
+            Dictionary<String, String> processList = getTaskbarProcessNames();
+            appComboBox.Items.Clear();
+            appComboBox.Items.Add(DefaultApplicationName);
+
+            foreach (string processName in processList.Keys)
+            {
+                appComboBox.Items.Add(processName);
+            }
+        }
+
+        private Dictionary<String, String> getTaskbarProcessNames()
+        {
+            Process[] processList = Process.GetProcesses();
+            Dictionary<String, String> result = new Dictionary<String, String>();
+
+            foreach (Process process in processList)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    String value;
+                    if (!result.TryGetValue(process.ProcessName, out value))
+                    {
+                        result.Add(process.ProcessName, process.ProcessName);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            resetGestureFields();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedItem != DefaultApplicationName)
+            {
+                //dummyProfile.UpdateGestureMapping(Gestures.Up, upTextBox.Text);
+                //dummyProfile.UpdateGestureMapping(Gestures.Down, downTextBox.Text);
+                //dummyProfile.UpdateGestureMapping(Gestures.Left, leftTextBox.Text);
+                //dummyProfile.UpdateGestureMapping(Gestures.Right, rightTextBox.Text);
+
+                CreateDummyMap();
+                MessageBox.Show("Saved");
+            }    
+        }
+
+        private void appComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String item = (String)appComboBox.SelectedItem;
+            
+            if (!selectedItem.Equals(item))
+            {
+ //               selectedItem = item;
+                //selectedMap = dummyMap;
+                if (selectedItem != DefaultApplicationName)
+                {
+                    /*
+                    Dictionary<String, String> map;                
+                    if (GTSettings.Settings.Instance.Mappings.Map.TryGetValue(selectedItem, map))
+                    {
+                        selectedMap = map;
+                    }
+                */
+                }                
+            }            
+        }
+
+        private void CreateDummyMap()
+        { 
+        
+        }
+
+
+
+
+
+
     }
 }
