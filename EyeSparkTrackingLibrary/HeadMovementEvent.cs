9b75af909ee04294030ce5c8465dea0f1e705028
Alex <alex.suarez100@gmail.com>
2012-11-24 21:43:15 -0400

    Hooked up stub head traking event

---
+++ EyeSparkTrackingLibrary/HeadMovementEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EyeSparkTrackingLibrary
+{
+    public delegate void HeadMovementEventHandler(object sender,
+       HeadMovementEventArgs e);
+
+    public class HeadMovementEventArgs : EventArgs
+    {
+    }
+}
--- EyeSparkTrackingLibrary/HeadTracker.cs
+++ EyeSparkTrackingLibrary/HeadTracker.cs
@@ -7,8 +7,9 @@ using System.Diagnostics;
 
 namespace EyeSparkTrackingLibrary
 {
-    class HeadTracker
+    public class HeadTracker
     {
+        #region Imports
         [DllImport("user32.dll")]
         private static extern IntPtr GetForegroundWindow();
 
@@ -18,14 +19,20 @@ namespace EyeSparkTrackingLibrary
         [DllImport("user32.dll")]
         private static extern int GetWindowText(IntPtr hWnd,
             StringBuilder buffer, int length);
+        #endregion
 
-        private HeadTracker instance;
+        #region Fields
+        private static HeadTracker instance;
+        #endregion
 
+        #region Constructor
         private HeadTracker() { }
+        #endregion
 
-        public HeadTracker Instance
+        #region Properties
+        public static HeadTracker Instance
         {
-            get 
+            get
             {
                 if (instance == null)
                 {
@@ -34,41 +41,65 @@ namespace EyeSparkTrackingLibrary
                 return instance;
             }
         }
+        #endregion
 
-        public void start() { 
-            // do the tracking here
+        #region Events
+        public event HeadMovementEventHandler HeadMovement;
+        #endregion
 
-//            while (true) 
-//            {
-                // we should get a reference to the device
-                // via the usb event.
-                if ("DEVICE" != null)
-                {
-                    // get window title
-                    IntPtr handle = GetForegroundWindow();
-                    int length = GetWindowTextLength(handle) + 1;
-                    StringBuilder title = new StringBuilder(length);
-                    GetWindowText(handle, title, title.Capacity);
+        public void Start()
+        {
+            System.Threading.Thread t = new System.Threading.Thread(StartThread);
+            t.Start();
+            //// do the tracking here
 
-                    // get processes
-                    Process[] pl = Process.GetProcesses();
-                    foreach (Process p in pl)
-                    {
-                        // find the process for the active window
-                        if (p.MainWindowHandle == handle)
-                        {
-                            //AppProfile profile;
-                            //if (profiles.TryGetValue(p.ProcessName, out profile))
-                            //{
-                            //    string randGesture = gestures[random.Next(gestures.Length)];
-                                
-                            //    // TODO: fire off an event and let the main window handle it
-                            //    //SendKeys.SendWait(profile.GetGestureMapping(randGesture));
+            ////            while (true) 
+            ////            {
+            //// we should get a reference to the device
+            //// via the usb event.
+            //if ("DEVICE" != null)
+            //{
+            //    // get window title
+            //    IntPtr handle = GetForegroundWindow();
+            //    int length = GetWindowTextLength(handle) + 1;
+            //    StringBuilder title = new StringBuilder(length);
+            //    GetWindowText(handle, title, title.Capacity);
 
-                            //}
-                        }
-                    }
-//                }            
+            //    // get processes
+            //    Process[] pl = Process.GetProcesses();
+            //    foreach (Process p in pl)
+            //    {
+            //        // find the process for the active window
+            //        if (p.MainWindowHandle == handle)
+            //        {
+            //            //AppProfile profile;
+            //            //if (profiles.TryGetValue(p.ProcessName, out profile))
+            //            //{
+            //            //    string randGesture = gestures[random.Next(gestures.Length)];
+
+            //            //    // TODO: fire off an event and let the main window handle it
+            //            //    //SendKeys.SendWait(profile.GetGestureMapping(randGesture));
+
+            //            //}
+            //        }
+            //    }
+                //                }            
+    //        }
+        }
+
+        private void StartThread()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(5000);
+                OnHeadMovement(null);
+            }
+        }
+
+        private void OnHeadMovement(HeadMovementEventArgs e) {
+            if (HeadMovement != null)
+            {
+                HeadMovement(this, e);
             }
         }
     }
