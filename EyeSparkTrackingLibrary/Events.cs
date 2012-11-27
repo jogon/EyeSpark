c42166d807f6b638a3f26b5500126d3ab1080beb
Alex <alex.suarez100@gmail.com>
2012-11-27 17:19:43 -0400

    Missing file from last commit

---
+++ EyeSparkTrackingLibrary/Events.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EyeSparkTrackingLibrary
+{
+    public delegate void HeadMovementEventHandler(object sender,
+       HeadMovementEventArgs e);
+
+    public delegate void HeadMeasurementEventHandler(object sender,
+       HeadMeasurementEventArgs e);
+
+
+    public class HeadMovementEventArgs : EventArgs
+    {
+        private string gesture;
+
+        public String Gesture
+        {
+            get 
+            {
+                return gesture;
+            }
+        }
+        public HeadMovementEventArgs(String gesture)
+        {
+            this.gesture = gesture;
+        }
+    }
+
+    public class HeadMeasurementEventArgs : EventArgs
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+
+        public Int16 X { get; set; }
+
+        public Int16 Y { get; set; }
+
+        public Int16 Z { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public HeadMeasurementEventArgs(Int16 x, Int16 y, Int16 z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        #endregion
+
+        
+    }
+}
